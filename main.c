047189116da0e9cb43fe90edc9f9e9f7bd4c3073
hardik500 <hardikkhandelwal500@gmail.com>
2019-10-13 18:26:49 +0530

    Added Files Listing and Automation of FTP

--- main.c
+++ main.c
@@ -1,32 +1,162 @@
 #include <sys/wait.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <regex.h>
 
+//variables and fuctions required for list command
+struct stat s;
+
+regex_t regex;
+const char *expression = "^[.]*$";
+int reti;
+
+//Directory listing main loop
+int listDirectory(int counter, int level, char arr[200], char space[100])
+{
+  // printf("Here Counter = %d and Level = %d and directory is %s\n", counter, level, arr);
+  printf("%s|_%s\n", space, arr);
+
+  //Character for seperating each directories or files
+  char c = '/';
+
+  //Get the length of the current directory
+  size_t len = strlen(arr);
+
+  //Declare a new string with size of the root + that of the char c
+  char *modifiedRootDir = malloc(len + 1 + 1);
+
+  //Copy the name of cuurentDirectory in new String
+  strcpy(modifiedRootDir, arr);
+
+  modifiedRootDir[len] = c;
+  modifiedRootDir[len + 1] = '\0';
+
+  //Check if the counter is equal to or greater than level. If yes exit
+  if (counter >= level)
+  {
+    return 0;
+  }
+
+  //Store the pointer to the currentDirectory in a new variable
+  char *currentDirctory = modifiedRootDir;
+
+  // Pointer for directory entry
+  struct dirent *de;
+
+  // opendir() returns a pointer of DIR type.
+  DIR *dr = opendir(currentDirctory);
+
+  // opendir returns NULL if couldn't open directory i.e. it is a file
+  if (dr == NULL)
+  {
+    // printf("\nCould not open current directory\n");
+    return 0;
+  }
+
+  //Declare root variable
+  char root[100];
+
+  //Copy
+  strcpy(root, modifiedRootDir);
+
+  char newSpace[200];
+  char lengthOfSpace[100] = "  ";
+  strcat(newSpace, space);
+  strcat(newSpace, lengthOfSpace);
+
+  //Read the contents of the directory
+  while ((de = readdir(dr)) != NULL)
+  {
+    int size = 0;
+
+    struct stat statbuf;
+
+    //The name of each file or folder
+    char *x = de->d_name;
+    char c2[100];
+
+    strcpy(c2, x);
+
+    strcpy(currentDirctory, c2);
+
+    //Replace the content of arr with that of new directory or file name
+    reti = regexec(&regex, currentDirctory, 0, NULL, 0);
+
+    /*Check if the file doesn't match . or .. which is the parent and grand-parent directory*/
+    if (!reti)
+    {
+      //If it matches check for next file
+      continue;
+    }
+    else if (reti == REG_NOMATCH)
+    {
+
+      //Check if the file or the path is a direcfory
+      stat(arr, &statbuf);
+
+      /*If it is a directory then increase the counter and size and put the directory in loop for next call*/
+      counter += 1;
+
+      size_t lengthOfRoot = strlen(root);
+      size_t lengthOfCurrentDir = strlen(currentDirctory);
+
+      /*Set the length of the new directory which is equal to the length of the previous root directory*/
+      /*as well as the length of the new directory that is found*/
+      char *newDir = malloc(lengthOfRoot + lengthOfCurrentDir + 1);
+
+      //Copy the contents of the root directory in a new directory
+      strcpy(newDir, root);
+
+      //Concat the root directory and new directory
+      strcat(newDir, currentDirctory);
+
+      /*At the end of the string set a null character which specifies the end of the string*/
+      newDir[lengthOfCurrentDir + lengthOfRoot + 1] = '\0';
+
+      //Go into loop with new counter, directory name, and new space
+      listDirectory(counter, level, newDir, newSpace);
+
+      /*Increase the value of the size which store the number of times the loops occurs*/
+      size += 1;
+
+      //Decrease the size of the counter equal to times the loops occurs
+      counter -= size;
+    }
+  }
+  closedir(dr);
+}
 /*
   Function Declarations for builtin shell commands:
  */
+int lsh_autoftp(char **args);
 int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
-
+int lsh_list(char **args);
 /*
   List of builtin commands, followed by their corresponding functions.
  */
 char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit"
-};
+    "autoftp",
+    "cd",
+    "help",
+    "exit",
+    "list"};
 
-int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
-};
+int (*builtin_func[])(char **) = {
+    &lsh_autoftp,
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit,
+    &lsh_list};
 
-int lsh_num_builtins() {
+int lsh_num_builtins()
+{
   return sizeof(builtin_str) / sizeof(char *);
 }
 
@@ -35,16 +165,20 @@ int lsh_num_builtins() {
 */
 
 /**
-   @brief Bultin command: change directory.
-   @param args List of args.  args[0] is "cd".  args[1] is the directory.
-   @return Always returns 1, to continue executing.
+  @brief Bultin command: change directory.
+  @param args List of args. two arguments args[0] is "cd".  args[1] is the directory.
+  @return Always returns 1, to continue executing.
  */
 int lsh_cd(char **args)
 {
-  if (args[1] == NULL) {
+  if (args[1] == NULL)
+  {
     fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
+  }
+  else
+  {
+    if (chdir(args[1]) != 0)
+    {
       perror("lsh");
     }
   }
@@ -52,17 +186,19 @@ int lsh_cd(char **args)
 }
 
 /**
-   @brief Builtin command: print help.
-   @param args List of args.  Not examined.
-   @return Always returns 1, to continue executing.
+  @brief Builtin command: print help.
+  @param args List of args. only args[0]=help.
+  @return Always returns 1, to continue executing.
  */
 int lsh_help(char **args)
 {
   int i;
+  printf("LSH\n");
   printf("Type program names and arguments, and hit enter.\n");
   printf("The following are built in:\n");
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
+  for (i = 0; i < lsh_num_builtins(); i++)
+  {
     printf("  %s\n", builtin_str[i]);
   }
 
@@ -71,15 +207,85 @@ int lsh_help(char **args)
 }
 
 /**
-   @brief Builtin command: exit.
-   @param args List of args.  Not examined.
-   @return Always returns 0, to terminate execution.
+  @brief Builtin command: exit.
+  @param args List of args.  only args[0]=exit.
+  @return Always returns 0, to terminate execution.
  */
 int lsh_exit(char **args)
 {
   return 0;
 }
+/**
+  @brief Bultin command: list files.
+  @param args List of args. only args[0] is "list"
+  @return Always returns 1, to continue executing.
+ */
 
+//Code for directory listing
+int lsh_list(char **args)
+{
+  //This is the root directory
+  char arr[200] = ".";
+
+  if (args[1] == NULL)
+  {
+    fprintf(stderr, "lsh: expected argument to \"list\" e.g. list 1\n");
+  }
+  else
+  {
+    //Get the level from the argument
+    int level = atoi(args[1]);
+
+    //Compile the regular expression
+    reti = regcomp(&regex, expression, 0);
+    if (reti)
+    {
+      fprintf(stderr, "Could not compile regex\n");
+    }
+
+    //Go into the main loop
+    listDirectory(0, level, arr, "");
+
+    return 1;
+  }
+}
+/**
+  @brief Bultin command: change directory.
+  @param args List of args. only args[0] is "autoftp".
+  @return Always returns 1, to continue executing.
+ */
+
+// Code for autoftp
+int lsh_autoftp(char **args)
+{
+  //File Pointerr for the bash script
+  FILE *ftpFile;
+
+  //Open the file in read mode
+  ftpFile = popen("./f.sh", "r");
+
+  if (ftpFile != NULL)
+  {
+    while (1)
+    {
+      char *line;
+      char buf[1000];
+
+      //Get each line
+      line = fgets(buf, sizeof buf, ftpFile);
+      if (line == NULL)
+      {
+        break;
+      }
+      else
+        printf("%s", line);
+    }
+
+    //Close the pointer
+    pclose(ftpFile);
+  }
+  return 1;
+}
 /**
   @brief Launch a program and wait for it to terminate.
   @param args Null terminated list of arguments (including program).
@@ -87,45 +293,55 @@ int lsh_exit(char **args)
  */
 int lsh_launch(char **args)
 {
-  pid_t pid, wpid;
+  pid_t pid;
   int status;
 
   pid = fork();
-  if (pid == 0) {
+  if (pid == 0)
+  {
     // Child process
-    if (execvp(args[0], args) == -1) {
+    if (execvp(args[0], args) == -1)
+    {
       perror("lsh");
     }
     exit(EXIT_FAILURE);
-  } else if (pid < 0) {
+  }
+  else if (pid < 0)
+  {
     // Error forking
     perror("lsh");
-  } else {
+  }
+  else
+  {
     // Parent process
-    do {
-      wpid = waitpid(pid, &status, WUNTRACED);
+    do
+    {
+      waitpid(pid, &status, WUNTRACED);
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
   }
-
   return 1;
 }
 
 /**
-   @brief Execute shell built-in or launch program.
-   @param args Null terminated list of arguments.
-   @return 1 if the shell should continue running, 0 if it should terminate
- */
+  @brief Execute shell built-in or launch program.
+  @param args Null terminated list of arguments.
+  @return 1 if the shell should continue running, 0 if it should terminate
+*/
+
 int lsh_execute(char **args)
 {
   int i;
 
-  if (args[0] == NULL) {
+  if (args[0] == NULL)
+  {
     // An empty command was entered.
     return 1;
   }
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
-    if (strcmp(args[0], builtin_str[i]) == 0) {
+  for (i = 0; i < lsh_num_builtins(); i++)
+  {
+    if (strcmp(args[0], builtin_str[i]) == 0)
+    {
       return (*builtin_func[i])(args);
     }
   }
@@ -135,8 +351,8 @@ int lsh_execute(char **args)
 
 #define LSH_RL_BUFSIZE 1024
 /**
-   @brief Read a line of input from stdin.
-   @return The line from stdin.
+  @brief Read a line of input from stdin.
+  @return The line from stdin.
  */
 char *lsh_read_line(void)
 {
@@ -145,29 +361,39 @@ char *lsh_read_line(void)
   char *buffer = malloc(sizeof(char) * bufsize);
   int c;
 
-  if (!buffer) {
+  if (!buffer)
+  {
     fprintf(stderr, "lsh: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
-  while (1) {
+  while (1)
+  {
     // Read a character
     c = getchar();
 
-    // If we hit EOF, replace it with a null character and return.
-    if (c == EOF || c == '\n') {
+    if (c == EOF)
+    {
+      exit(EXIT_SUCCESS);
+    }
+    else if (c == '\n')
+    {
       buffer[position] = '\0';
       return buffer;
-    } else {
+    }
+    else
+    {
       buffer[position] = c;
     }
     position++;
 
     // If we have exceeded the buffer, reallocate.
-    if (position >= bufsize) {
+    if (position >= bufsize)
+    {
       bufsize += LSH_RL_BUFSIZE;
       buffer = realloc(buffer, bufsize);
-      if (!buffer) {
+      if (!buffer)
+      {
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
       }
@@ -178,30 +404,36 @@ char *lsh_read_line(void)
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
 /**
-   @brief Split a line into tokens (very naively).
-   @param line The line.
-   @return Null-terminated array of tokens.
+  @brief Split a line into tokens (very naively).
+  @param line The line.
+  @return Null-terminated array of tokens.
  */
 char **lsh_split_line(char *line)
 {
   int bufsize = LSH_TOK_BUFSIZE, position = 0;
-  char **tokens = malloc(bufsize * sizeof(char*));
-  char *token;
+  char **tokens = malloc(bufsize * sizeof(char *));
+  char *token, **tokens_backup;
 
-  if (!tokens) {
+  if (!tokens)
+  {
     fprintf(stderr, "lsh: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
   token = strtok(line, LSH_TOK_DELIM);
-  while (token != NULL) {
+  while (token != NULL)
+  {
     tokens[position] = token;
     position++;
 
-    if (position >= bufsize) {
+    if (position >= bufsize)
+    {
       bufsize += LSH_TOK_BUFSIZE;
-      tokens = realloc(tokens, bufsize * sizeof(char*));
-      if (!tokens) {
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char *));
+      if (!tokens)
+      {
+        free(tokens_backup);
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
       }
@@ -214,7 +446,7 @@ char **lsh_split_line(char *line)
 }
 
 /**
-   @brief Loop getting input and executing it.
+  @brief Loop getting input and executing it.
  */
 void lsh_loop(void)
 {
@@ -222,7 +454,8 @@ void lsh_loop(void)
   char **args;
   int status;
 
-  do {
+  do
+  {
     printf("> ");
     line = lsh_read_line();
     args = lsh_split_line(line);
@@ -234,10 +467,10 @@ void lsh_loop(void)
 }
 
 /**
-   @brief Main entry point.
-   @param argc Argument count.
-   @param argv Argument vector.
-   @return status code
+  @brief Main entry point.
+  @param argc Argument count.
+  @param argv Argument vector.
+  @return status code
  */
 int main(int argc, char **argv)
 {
